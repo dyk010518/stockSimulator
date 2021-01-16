115e47307f0cbcda09abf8229341bec14917c67d
RonaldBXu <goronaldxu@gmail.com>
2021-01-15 21:28:24 -0500

    login request added

--- src/Homepage/TopBar/Login/Login.js
+++ src/Homepage/TopBar/Login/Login.js
@@ -6,14 +6,16 @@ class Login extends Component {
   constructor(props) {
     super(props);
   }
-
+  loginRequest = () => {
+		alert("login request!")
+	};
   // required method: whatever is returned defines what
   // shows up on screen
   render() {
     return (
       <>
-      <button className="loginButton" >Login</button>
-      </>
+			<button className="loginButton" onClick={this.loginRequest}>Login</button>
+			</>
     );
   }
 }
