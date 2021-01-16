85e471db32ac118a55d45dab534fcb888602ec15
joehyunkim5 <jenblue@mit.edu>
2021-01-16 11:57:57 -0500

    Login

--- src/Homepage/TopBar/Login/Login.js
+++ src/Homepage/TopBar/Login/Login.js
@@ -1,21 +1,27 @@
 import './Login.css';
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import GoogleLogin from 'react-google-login';
+
 
 class Login extends Component {
   // makes props available in this component
   constructor(props) {
     super(props);
+    this.loginRequest = this.loginRequest.bind(this);
   }
-  loginRequest = () => {
-		alert("login request!")
+
+  loginRequest = (response) => {
+		console.log("Successfully logged in");
+    console.log(response);
 	};
   // required method: whatever is returned defines what
   // shows up on screen
   render() {
     return (
-      <>
-			<button className="loginButton" onClick={this.loginRequest}>Login</button>
-			</>
+      <Fragment>
+        <GoogleLogin className="loginButton" clientId="861706660529-n3glom9el42gfl6pcs34ll6adf1v4ci2.apps.googleusercontent.com"
+          buttonText= "Login" onSuccess={this.loginRequest} onFailure={this.longinRequest}/>
+      </Fragment>
     );
   }
 }
