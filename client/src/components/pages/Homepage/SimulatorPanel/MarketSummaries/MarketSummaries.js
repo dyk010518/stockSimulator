2a56b9d8bd06ba104bbefaab6996570398a6cc96
joehyunkim5 <jenblue@mit.edu>
2021-01-18 09:39:23 -0500

    added market buttons for market 2,3, and 4

--- client/src/components/pages/Homepage/SimulatorPanel/MarketSummaries/MarketSummaries.js
+++ client/src/components/pages/Homepage/SimulatorPanel/MarketSummaries/MarketSummaries.js
@@ -6,11 +6,14 @@ import {Link} from '@reach/router';
 import Market from '../../../Market/Market.js'
 import App from '../../../../App.js';
 
+const marketTypes = ["One", "Two", "Three", "Four"];
+
 class MarketSummaries extends Component {
 	// makes props available in this component
 	constructor(props) {
 		super(props);
 	}
+
 
 	// required method: whatever is returned defines what
 	// shows up on screen
@@ -21,13 +24,17 @@ class MarketSummaries extends Component {
 					Markets
         		</p>
 				<div className="MarketPanel-grid">
-				<Link to="/Game" class="marketButton" ><button onClick={()=>{
-					this.props.setMarketNum("One")
-					
-				}}>Market 1</button></Link>
-				<Link to="/Game" class="marketButton" marketNumber="Two">Market 2</Link>
-				<Link to="/Game" class="marketButton" marketNumber="Three">Market 3</Link>
-				<Link to="/Game" class="marketButton" marketNumber="Four">Market 4</Link>
+
+          {
+            marketTypes.map((marketNumber) => (
+              <Link to="/Game" class="marketButton" >
+                <button onClick={()=>{this.props.setMarketNum(marketNumber)}}>
+                  Market {marketNumber}
+                </button>
+              </Link>
+            ))
+          }
+
 				</div>
 			</div>
 		);
