3bafc1ccd22bc9a3825832dea4d4e44829a510a3
joehyunkim5 <jenblue@mit.edu>
2021-01-18 09:55:48 -0500

    added home button in market navbar

--- client/src/components/pages/Market/MarketNavBar/MarketNavBar.js
+++ client/src/components/pages/Market/MarketNavBar/MarketNavBar.js
@@ -8,6 +8,10 @@ import MarketPortfolio from './MarketPortfolio.js';
 import MarketResearch from './MarketResearch.js';
 import MarketTrade from './MarketTrade.js';
 
+const routes = [{link: "/", text: "Home"},{link: "/Game/Dashboard", text: "Dashboard"},
+{link: "/Game/Portfolio", text: "Portfolio"}, {link: "/Game/Research", text: "Stock Research"}, 
+{link: "/Game/Trade", text: "Trade"}];
+
 class MarketNavBar extends Component {
     // makes props available in this component
     constructor(props) {
@@ -21,27 +25,18 @@ class MarketNavBar extends Component {
             <>
                 <header className="MNavBar-header">
                     <div className ="allButtons">
-                    <div className="section">
-                        <Link to="/Game/Dashboard" className="rm_decor">Dashboard</Link>
-                    </div>
-                    <div className="section">
-                        |
-                    </div>
-                    <div className="section">
-                        <Link to="/Game/Portfolio" className="rm_decor">Portfolio</Link>
-                    </div>
-                    <div className="section">
-                        |
-                    </div>
-                    <div className="section">
-                        <Link to="/Game/Research" className="rm_decor">Stock Research</Link>
-                    </div >
-                    <div className="section">
-                        |
-                    </div>
-                    <div className="section">
-                    <Link to="/Game/Trade" className="rm_decor">Trade</Link>
-                    </div>
+                      {
+                        routes.map((route) => (
+                          <>
+                            <div className="section">
+                                <Link to={route.link} className="rm_decor">{route.text}</Link>
+                            </div>
+                            <div className="section">
+                                |
+                            </div>
+                          </>
+                          ))
+                      }
                     </div>
                 </header>
             </>
