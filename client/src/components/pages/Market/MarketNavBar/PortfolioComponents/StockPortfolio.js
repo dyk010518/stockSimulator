d3635ded41ac82653883c1f2f484e878271c9e64
dyk010518 <dyk010518@naver.com>
2021-01-26 15:42:50 -0500

    Portfolio works now

--- client/src/components/pages/Market/MarketNavBar/PortfolioComponents/StockPortfolio.js
+++ client/src/components/pages/Market/MarketNavBar/PortfolioComponents/StockPortfolio.js
@@ -12,6 +12,7 @@ class StockPortfolio extends Component {
   }
 
   state = {
+    stockDay: undefined,
     userCash: {
       cashValue: undefined,
       accountTotal: undefined,
@@ -70,28 +71,90 @@ class StockPortfolio extends Component {
     if (this.props.id) {
       let theStocks = [];
       let theObject = {};
-      get('/api/boughtstocks', { id: this.props.id }).then((boughtStockObjs) => {
-        for (let i = 0; i < boughtStockObjs.length; i++) {
-          theObject = {
-            stockSymbol: boughtStockObjs[i].stockName,
-            lastPrice: 10000.0,
-            todayDollarChange: 10000.0,
-            todayPercentChange: 56.5,
-            totalDollarChange: 112300.0,
-            totalPercentChange: 156.5,
-            currentValue: 12459071,
-            accountPercent: 47,
-            quantity: 1208,
-            costBasisPS: 1234,
-            costBasis: 1084,
-          }
-
-          theStocks.push(theObject);
-        }
+      
+      get('/api/getdate', {id: this.props.id }).then((dateObj) => {
+        let tempDay = dateObj.one;
         this.setState({
-          stocks: theStocks,
-        }, () => { console.log(this.state.stocks) })
-      });
+          stockDay: tempDay,
+        }, () => {
+          get('/api/boughtstocks', { id: this.props.id }).then((boughtStockObjs) => {
+            for (let i = 0; i < boughtStockObjs.length; i++) {
+              get("/api/getPriceData", {
+                symbol: boughtStockObjs[i].stockName,
+                day: tempDay,
+                // below is hard-coded market number
+                number: "1",
+              }).then((stockObj) => {    
+                let priceChange = 0;
+                let yesterdayPrice = 1;
+                get("/api/getPriceData", {
+                  symbol: boughtStockObjs[i].stockName,
+                  day: tempDay-1,
+                  // below is hard-coded market number
+                  number: "1",
+                }).then((yesterStock) => {   
+                  if(yesterStock.obj !== undefined){
+                    if((stockObj.obj.stockPrice !== boughtStockObjs[i].costBasis)){
+                      let rawChange = parseFloat(stockObj.obj.stockPrice) - parseFloat(yesterStock.obj.stockPrice)
+                      priceChange = (Math.round(parseFloat(rawChange) * 100) / 100);
+                      yesterdayPrice = (Math.round(parseFloat(yesterStock.obj.stockPrice) * 100) / 100);
+                    }
+                  }
+                  let thePrice = (Math.round(parseFloat(stockObj.obj.stockPrice) * 100) / 100);
+                  let theQuantity = (Math.round(parseFloat(boughtStockObjs[i].quantity) * 100) / 100);
+                  let theCostBasis = (Math.round(parseFloat(boughtStockObjs[i].costBasis) * 100) / 100);
+                  
+                  let todayDollor = (Math.round(parseFloat(priceChange*theQuantity) * 100) / 100);
+                  let todayPercent = priceChange/yesterdayPrice;
+                  todayPercent = (Math.round(parseFloat(todayPercent) * 10000) / 100);
+
+                  let currentVal = (Math.round(parseFloat(thePrice*theQuantity) * 100) / 100);
+
+                  let stockPercentage = (parseFloat(thePrice*theQuantity) / parseFloat(this.props.totalValue));
+                  stockPercentage = (Math.round(parseFloat(stockPercentage) * 10000) / 100);
+
+                  let totalCostBasis = (Math.round(parseFloat(theQuantity*theCostBasis) * 100) / 100);
+
+                  let totalDollor = (Math.round(parseFloat((currentVal-totalCostBasis)) * 100) / 100);
+                  let totalPercent = (Math.round(parseFloat(totalDollor/totalCostBasis) * 10000) / 100);
+
+
+
+                  theObject = {
+                    stockSymbol: boughtStockObjs[i].stockName,
+                    lastPrice: "$" + thePrice.toString(),
+                    todayDollarChange: "$" + todayDollor.toString(),
+                    todayPercentChange: todayPercent.toString() + "%",
+                    totalDollarChange: "$" + totalDollor,
+                    totalPercentChange: totalPercent + "%",
+                    currentValue: "$" + currentVal.toString(),
+                    accountPercent: stockPercentage.toString() + "%",
+                    quantity: theQuantity.toString(),
+                    costBasisPS: "$" + theCostBasis.toString(),
+                    costBasis: "$" + totalCostBasis.toString(),
+                  }      
+                  theStocks.push(theObject);
+                  this.setState({
+                    stocks: theStocks,
+                  }, () => {console.log(this.state.stocks)});
+                })
+
+                
+              })
+            }
+
+              
+          });
+            
+        })
+      })
+      .then(() => {
+        // this.setState({
+        //   stocks: theStocks,
+        // }, () => {console.log(this.state.stocks)});
+      })
+
+      
     }
   }
 
